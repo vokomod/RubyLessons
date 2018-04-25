682119f676cea621676cd4e3383c1a5a59402242
vokomod <vokomod@gmail.com>
2018-04-25 15:36:11 +0300

    Add submit

--- app40/barbershop.rb
+++ app40/barbershop.rb
@@ -3,3 +3,16 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+  @userName = params[:userName]
+  @userPhoneNumber = params[:userPhoneNumber]
+  @dateAndTime = params[:dateAndTime]
+
+  erb :message
+
+  @title = "Hello #{@userName}, you are welcome!!"
+  @message = "Your phone number #{@userPhoneNumber} is correct?
+              We are waitnig for you at #{@dateAndTime}"
+
+end
