fa5ce329aad9521a55d504e34738ef4f09ed7867
vokomod <vokomod@gmail.com>
2018-04-25 19:10:10 +0300

    add barbershop2

---
+++ app41/barbershop2.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'tilt/erubis'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @userName = params[:userName]
+  @userPhoneNumber = params[:userPhoneNumber]
+  @dateAndTime = params[:dateAndTime]
+
+  @title = "Hello #{@userName}, you are welcome!!"
+  @message = "Your phone number #{@userPhoneNumber} is correct?
+              We are waitnig for you at #{@dateAndTime}"
+
+  fileDataInput = File.open './public/users.txt', 'a'
+  fileDataInput.write "User: #{@userName}, phone: #{@userPhoneNumber}, date: #{@dateAndTime}\n"
+  fileDataInput.close
+
+  erb :message
+end
+
+get '/admin' do
+  erb :admin
+end
+
+post '/admin' do
+  @adminPassword = params[:adminPassword]
+
+  if @adminPassword.strip == "zzz"
+
+    erb :data
+
+  end
+end
