b76450538ada6f5d5d113f4283b481a345a3281e
vokomod <vokomod@gmail.com>
2018-05-03 18:13:37 +0300

    add my repo

---
+++ lesson21/app42-bootstrapruby/app.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/login/form' do
+  erb :login_form
+end
+
+post '/login/attempt' do
+  pass = params['password']
+  session[:identity] = params['username']
+  if pass == "secret" && params['username'] == "admin"
+    where_user_came_from = session[:previous_url] || '/'
+    redirect to where_user_came_from
+  else
+    erb "<div class='alert alert-primary'>Get off!</div>"
+  end
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+get '/secure/place' do
+  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+end
